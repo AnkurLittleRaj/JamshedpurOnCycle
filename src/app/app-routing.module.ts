671ac86aaa26c342e7e48d8b6943cf58ef744e6d
Ankur Raj <raj.ankur8@gmail.com>
2021-11-16 19:07:45 +0530

    first commit 2nd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { AreaDetailComponent } from '../components/area-detail/area-detail.component';
+import { MainPageBodyComponent } from '../components/main-page-body/main-page-body.component';
+const routes: Routes = [
+  {path:'',component:MainPageBodyComponent,pathMatch:"full"},
+  {path:'areaDetail',component:AreaDetailComponent,pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from '../components/header/header.component';
+import { MainPageBodyComponent } from '../components/main-page-body/main-page-body.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule,} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { AreaDetailComponent } from '../components/area-detail/area-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageBodyComponent,
+    AreaDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
